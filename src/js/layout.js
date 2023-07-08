b4aeb41d91ca775a4a6feac8b5a595456223c27d
JohnCritch90 <123122544+JohnCritch90@users.noreply.github.com>
2023-07-08 06:39:08 +0000

    Meh

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,37 +1,34 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
-
-import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Navbar } from "./component/navbar";
+import { Main } from "./views/main";
+
+import { Character } from "./views/character";
+import { Planet } from "./views/planet";
+import { Vehicle } from "./views/vehicle";
+
+import injectContext from "./store/appContext";
 import { Footer } from "./component/footer";
 
-//create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+  return (
+    <div className="d-flex flex-column h-100">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
 
-	return (
-		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
-	);
+          <Route path="/character/:name" element={<Character />} />
+          <Route path="/planet/:name" element={<Planet />} />
+          <Route path="/vehicle/:name" element={<Vehicle />} />
+
+          <Route path="*" element={<h1>Not found!</h1>} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
